8d12c7c720fb72e99955cc51445a9857adc94767
Lucio <luciobp869@gmail.com>
2025-04-15 11:23:53 -0300

    Api fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_commerce/texto.dart';
+import 'package:my_commerce/produto.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -14,29 +17,51 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int contagem = 1;
+  Future<Produto> produtosFuture = getProdutos();
+  Future<String> prodFuture = Produto.produtoE();
+  static Future<Produto> getProdutos() async {
+    var url = Uri.parse("http://192.168.15.79:8000/api/produto/1");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return Produto.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
+    } else {
+      throw Exception('Não foi possivel carregar');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: Text('Meu primeiro app'),
         ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          addAutomaticKeepAlives: true,
-          children: [],
+        body: Center(
+          child: FutureBuilder<Produto>(
+            future: produtosFuture,
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasData) {
+                final produto = snapshot.data!;
+                return Text("${produto.nome}");
+              } else {
+                return const Text("Sem dados.");
+              }
+            },
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          
           backgroundColor: Colors.blue,
           onPressed: () {
             setState(() {
               contagem++;
             });
           },
-          child: Icon(Icons.add)
+          child: Icon(Icons.add),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
@@ -52,6 +77,16 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildProdutos(List<Produto> produtos) {
+    return ListView.builder(
+      itemCount: produtos.length,
+      itemBuilder: (context, index) {
+        final produto = produtos[index];
+        return Container(child: Row(children: [Text(produto.nome!)]));
+      },
     );
   }
 }
