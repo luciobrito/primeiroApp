bf00e5df2ea2f41ef70461badb3153a3e569e322
Lucio <luciobp869@gmail.com>
2025-04-21 21:41:24 -0300

    Página de cadastro de produto

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,17 +1,41 @@
 import 'dart:developer';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Cadastro extends StatelessWidget {
   const Cadastro({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final apiUrl = "http://192.168.15.79:8000/api/produto";
+    TextEditingController nomeController = TextEditingController();
+    TextEditingController precoController = TextEditingController();
+    TextEditingController codigoController = TextEditingController();
+    TextEditingController descricaoController = TextEditingController();
+    Future<void> sendRequest() async {
+      var response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "nome": nomeController.text,
+          "preco":  precoController.text,
+          "codigo_barra": codigoController.text,
+          "descricao": descricaoController.text,
+        }),
+      );
+      if(response.statusCode == 201){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Produto cadastrado com sucesso')));
+      }
+      else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Não foi possível cadastrar')));
+      }
+    }
+
     InputDecoration inputStyle(String label, String hint) => InputDecoration(
       hintText: hint,
       label: Text(label),
       border: OutlineInputBorder(),
-      
     );
     return Scaffold(
       appBar: AppBar(
@@ -21,25 +45,31 @@ class Cadastro extends StatelessWidget {
       body: Container(
         margin: EdgeInsets.all(12),
         child: Column(
+          spacing: 10,
           children: [
-            TextField(decoration: inputStyle('Nome', 'Maquiagem')),
             TextField(
+              controller: nomeController,
+              decoration: inputStyle('Nome', 'Maquiagem')),
+            TextField(
+              controller: precoController,
               keyboardType: TextInputType.number,
               decoration: inputStyle('Preço', '0.00'),
             ),
             TextField(
+              controller: codigoController,
               keyboardType: TextInputType.number,
               decoration: inputStyle('Código de Barra', '00000'),
             ),
             TextField(
+              controller: descricaoController,
               decoration: inputStyle('Descrição', 'Produto de alta qualidade'),
             ),
             ElevatedButton(
-              onPressed: () => log('Botão apertado'),
+              onPressed: sendRequest,
               child: Text('Cadastrar Produto'),
             ),
           ],
-          spacing: 10,
+     
         ),
       ),
     );
